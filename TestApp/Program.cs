6b58c4c8d38aea6ea6ca18f8418809b6fb8c835c
zhenyashumel <zhenyashumel10@gmail.com>
2024-01-19 19:47:59 +0100

    unfuck

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 
 var table = new DataTable();
-var result = table.Compute("3+3-(1+5) + 99.43", string.Empty);
+var result = table.Compute("3+3-(1+5) / 2 * 112 + 99.4344", string.Empty);
+Console.WriteLine("don't fuck");
 Console.Write(result);

18b379dde02c51c65843a243293145bcbc5151b5
zhenyashumel <zhenyashumel10@gmail.com>
2024-01-19 19:47:07 +0100

    changed expression

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,5 +2,6 @@
 
 Console.WriteLine("Welcome to me program");
 var table = new DataTable();
-var result = table.Compute("3+3-(1+5) + 99.43", string.Empty);
+var result = table.Compute("3+4-(1+5) + 99.43", string.Empty);
+Console.WriteLine("Fuck");
 Console.Write(result);

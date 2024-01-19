1f91b4a3efb226da5abc2d80a0117db1bd1518a7
zhenyashumel <zhenyashumel10@gmail.com>
2024-01-19 19:33:49 +0100

    added hello

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 
+Console.WriteLine("Welcome to me program");
 var table = new DataTable();
 var result = table.Compute("3+3-(1+5) + 99.43", string.Empty);
 Console.Write(result);
